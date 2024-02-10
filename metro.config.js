58868a23306cba311d6c1c84c9594952d75ba51f
zehrafzm <you@example.com>
2024-02-10 03:13:55 +0300

    firebase eklentili

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+
+
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push('cjs');
+
+module.exports = defaultConfig;
